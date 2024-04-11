5399d023d1d14d19d936c5d9c5471700f860f1ef
Abdulrahman Odhaib <157416943+Abdulrahman-odhaib@users.noreply.github.com>
2024-04-12 02:14:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,17 +9,17 @@ mkdir tools
 
 # Install Sublist3r
 echo "Installing Sublist3r..."
-cd /$USER/Multiple/tools
+cd tools/
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r
 sudo pip install -r requirements.txt
 sudo pip install requests
 sudo pip install dnspython
 sudo pip install argparse
+cd ..
 
 # Install Eyewitness
 echo "Installing Eyewitness..."
-cd /$USER/Multiple/tools
 git clone https://github.com/RedSiege/EyeWitness.git
 cd EyeWitness/Python/setup
 ./setup.sh
